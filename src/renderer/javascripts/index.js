bdba3c3f171d8edf90debcca80cdfc2b6a04c55c
Mateusz Kocz <mateusz@kocz.pl>
2020-06-11 17:39:08 +0200

    Add React and TS config

--- src/renderer/javascripts/index.js
+++ src/renderer/javascripts/index.js
@@ -1,8 +1,14 @@
 import { ipcRenderer } from 'electron'
+import React from 'react';
+import ReactDOM from "react-dom"
+import App from "./app.component.tsx"
+
 require('application.css')
 
 ipcRenderer.on('loaded', (event, data) => {
+  ReactDOM.render(<App/>, document.getElementById("app"))
   document.getElementById('title').innerHTML = data.appName + ' App 2'
   document.getElementById('details').innerHTML = 'built with Electron v' + data.electronVersion
   document.getElementById('versions').innerHTML = 'running on Node v' + data.nodeVersion + ' and Chromium v' + data.chromiumVersion
 })
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   renderer: {
-    entry: './src/renderer/javascripts/index.js'
+    entry: ['./src/renderer/javascripts/index.js'],
+    resolve: {
+      extensions: [".js", ".json", ".jsx", ".css", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-typescript"],
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        }
+      ]
+    },
   },
   preload: {
     entry: './src/preload/index.js'
