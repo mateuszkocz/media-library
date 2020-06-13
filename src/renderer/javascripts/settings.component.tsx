07ad163d3c6b8efa07c4af39d8b136e3107b4cc3
Mateusz Kocz <mateusz@kocz.pl>
2020-06-13 14:46:01 +0200

    Add settings store

--- src/renderer/javascripts/settings.component.tsx
+++ src/renderer/javascripts/settings.component.tsx
@@ -7,16 +7,49 @@ import {
   Switch,
   Text,
 } from "@chakra-ui/core"
-import React, { FunctionComponent } from "react"
+import Store from "electron-store"
+import React, {
+  ChangeEvent,
+  FunctionComponent,
+  useEffect,
+  useState,
+} from "react"
+
+const settingsStore = new Store({
+  name: "settings",
+  schema: {
+    autoplay: {
+      type: "boolean",
+      default: false,
+    },
+    dance: {
+      type: "boolean",
+      default: true,
+    },
+  },
+})
+
+const useStore = () => {
+  const [, render] = useState()
+  useEffect(() => settingsStore.onDidAnyChange(render), [])
+  return settingsStore
+}
 
 export const Settings: FunctionComponent<{ path: string }> = () => {
+  const store = useStore()
   return (
     <Stack>
       <Heading>Settings</Heading>
       <Box p={4} borderWidth="1px" rounded="lg">
         <Flex justify="space-between" align="center">
           <FormLabel htmlFor="autoplay">Autoplay</FormLabel>
-          <Switch id="autoplay" />
+          <Switch
+            id="autoplay"
+            isChecked={store.get("autoplay") as boolean}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              store.set("autoplay", event.target.checked)
+            }
+          />
         </Flex>
         <Text fontSize={"s"}>
           Play videos and music automatically after selecting it.
@@ -25,7 +58,13 @@ export const Settings: FunctionComponent<{ path: string }> = () => {
       <Box p={4} borderWidth="1px" rounded="lg">
         <Flex justify="space-between" align="center">
           <FormLabel htmlFor="dance">Make the application dance</FormLabel>
-          <Switch id="dance" />
+          <Switch
+            id="dance"
+            isChecked={store.get("dance") as boolean}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              store.set("dance", event.target.checked)
+            }
+          />
         </Flex>
         <Text fontSize={"s"}>When you feel like dancing.</Text>
       </Box>
