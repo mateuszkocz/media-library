0bc84acf3f202d1cfb1698fbf65fc943874bd141
Mateusz Kocz <mateusz@kocz.pl>
2020-06-17 20:22:53 +0200

    Add rx hooks dependency

--- src/renderer/javascripts/content.component.tsx
+++ src/renderer/javascripts/content.component.tsx
@@ -4,6 +4,7 @@ import { Settings } from "./settings.component"
 import { Videos } from "./videos.component"
 import { Router } from "@reach/router"
 import { getDatabase } from "./database"
+import { Provider } from "rxdb-hooks"
 
 const Main: FunctionComponent<{ path: string }> = () => {
   const addHero = async function () {
