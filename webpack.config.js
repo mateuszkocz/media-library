e3fb8249aa1ffb37b148362c9da61a091e892406
Mateusz Kocz <mateusz@kocz.pl>
2020-06-18 17:34:45 +0200

    Add some babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   renderer: {
-    entry: ['./src/renderer/javascripts/index.js'],
+    entry: ["./src/renderer/javascripts/index.js"],
     resolve: {
       extensions: [".js", ".json", ".jsx", ".css", ".ts", ".tsx"],
     },
@@ -13,16 +13,21 @@ module.exports = {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-react", "@babel/preset-typescript"],
-            }
-          }
-        }
-      ]
+              plugins: [
+                "@babel/plugin-proposal-optional-chaining",
+                "@babel/plugin-proposal-nullish-coalescing-operator",
+                "@babel/plugin-proposal-numeric-separator",
+              ],
+            },
+          },
+        },
+      ],
     },
   },
   preload: {
-    entry: './src/preload/index.js'
+    entry: "./src/preload/index.js",
   },
   main: {
-    entry: './src/main/index.js'
-  }
+    entry: "./src/main/index.js",
+  },
 }
