748262d3ade9feb6d3b7d78264dfdc7f823f6b4b
Mateusz Kocz <mateusz@kocz.pl>
2020-06-24 18:45:27 +0200

    Remove toggling menu

--- src/renderer/javascripts/main-menu.component.tsx
+++ src/renderer/javascripts/main-menu.component.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  Collapse,
   Divider,
   Flex,
   FormControl,
@@ -8,10 +7,9 @@ import {
   Input,
   Link,
   Stack,
-  useDisclosure,
 } from "@chakra-ui/core"
-import React, { FunctionComponent } from "react"
 import { Link as ReachLink } from "@reach/router"
+import React, { FunctionComponent } from "react"
 
 const MenuItem: FunctionComponent<{ to: string }> = ({
   to,
@@ -32,10 +30,6 @@ const MenuItem: FunctionComponent<{ to: string }> = ({
 )
 
 export const MainMenu: FunctionComponent = () => {
-  const { isOpen: libraryVisible, onToggle: toggleLibrary } = useDisclosure()
-  const { isOpen: videosVisible, onToggle: toggleVideos } = useDisclosure()
-  const { isOpen: musicVisible, onToggle: toggleMusic } = useDisclosure()
-  const { isOpen: imagesVisible, onToggle: toggleImages } = useDisclosure()
   return (
     <Stack minWidth="400px" p="3">
       <FormControl>
@@ -49,51 +43,10 @@ export const MainMenu: FunctionComponent = () => {
       <Divider />
       <Flex justify="space-between" direction="column" height="100%">
         <Stack>
-          <Stack>
-            <Button onClick={toggleLibrary}>Library</Button>
-            <Collapse isOpen={libraryVisible}>
-              <Stack>
-                <MenuItem to="/">All</MenuItem>
-                <Button>Favourites</Button>
-                <Button>Highest score</Button>
-                <Divider />
-              </Stack>
-            </Collapse>
-          </Stack>
-          <Stack>
-            <Button onClick={toggleVideos}>Videos</Button>
-            <Collapse isOpen={videosVisible}>
-              <Stack>
-                <MenuItem to="/videos">All</MenuItem>
-                <Button>Favourites</Button>
-                <Button>Highest score</Button>
-                <Divider />
-              </Stack>
-            </Collapse>
-          </Stack>
-
-          <Stack>
-            <Button onClick={toggleMusic}>Music</Button>
-            <Collapse isOpen={musicVisible}>
-              <Stack>
-                <MenuItem to="/music">All</MenuItem>
-                <Button>Favourites</Button>
-                <Button>Highest score</Button>
-                <Divider />
-              </Stack>
-            </Collapse>
-          </Stack>
-          <Stack>
-            <Button onClick={toggleImages}>Images</Button>
-            <Collapse isOpen={imagesVisible}>
-              <Stack>
-                <MenuItem to="/images">All</MenuItem>
-                <Button>Favourites</Button>
-                <Button>Highest score</Button>
-                <Divider />
-              </Stack>
-            </Collapse>
-          </Stack>
+          <MenuItem to="/">All</MenuItem>
+          <MenuItem to="/videos">Videos</MenuItem>
+          <MenuItem to="/music">Music</MenuItem>
+          <MenuItem to="/images">Images</MenuItem>
         </Stack>
         <Stack>
           <MenuItem to="/settings">Settings</MenuItem>
