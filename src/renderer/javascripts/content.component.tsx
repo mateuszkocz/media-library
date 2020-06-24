372707ce47017fb31f217eb679da18361a8dcf7c
Mateusz Kocz <mateusz@kocz.pl>
2020-06-24 18:39:33 +0200

    Add animations to the page changing

--- src/renderer/javascripts/content.component.tsx
+++ src/renderer/javascripts/content.component.tsx
@@ -1,29 +1,68 @@
-import { Box, Button } from "@chakra-ui/core"
-import { Router } from "@reach/router"
+import { Box } from "@chakra-ui/core"
+import { Router, Location } from "@reach/router"
 import React, { FunctionComponent } from "react"
 import { Settings } from "./settings.component"
 import { Videos } from "./videos.component"
+import { AnimatePresence, motion } from "framer-motion"
 
-const Main: FunctionComponent<{ path: string }> = () => {
-  return <div>Main</div>
+const FramerRouter: FunctionComponent = ({ children }) => (
+  <Location>
+    {({ location }) => (
+      <Box p="5" width="100%" position="relative">
+        <AnimatePresence initial={false}>
+          <Router key={location.key} location={location}>
+            {children}
+          </Router>
+        </AnimatePresence>
+      </Box>
+    )}
+  </Location>
+)
+
+const PageContainer = motion.custom(Box)
+
+const Page: FunctionComponent = ({ children }) => {
+  return (
+    <PageContainer
+      height={"80vh"}
+      width={"100%"}
+      initial={{ x: "100%", opacity: 1, position: "absolute" }}
+      animate={{ x: 0, opacity: 1, position: "relative" }}
+      exit={{ x: "-100%", opacity: 0, position: "absolute" }}
+    >
+      {children}
+    </PageContainer>
+  )
 }
 
-const Images: FunctionComponent<{ path: string }> = () => {
-  return <div>Images</div>
+const MainPage: FunctionComponent<{ path: string }> = () => {
+  return <Page>Main</Page>
 }
 
-const Music: FunctionComponent<{ path: string }> = () => {
-  return <div>Music</div>
+const ImagesPage: FunctionComponent<{ path: string }> = () => {
+  return <Page>Images</Page>
+}
+
+const VideosPage: FunctionComponent<{ path: string }> = () => {
+  return (
+    <Page>
+      <Videos />
+    </Page>
+  )
+}
+
+const MusicPage: FunctionComponent<{ path: string }> = () => {
+  return <Page>Music</Page>
 }
 
 export const Content: FunctionComponent = () => {
   return (
-    <Box as={Router} p="5" width="100%">
-      <Main path="/" />
-      <Videos path="/videos" />
+    <FramerRouter>
+      <MainPage path="/" />
+      <VideosPage path="/videos" />
       <Settings path="/settings" />
-      <Images path="/images" />
-      <Music path="/music" />
-    </Box>
+      <ImagesPage path="/images" />
+      <MusicPage path="/music" />
+    </FramerRouter>
   )
 }
